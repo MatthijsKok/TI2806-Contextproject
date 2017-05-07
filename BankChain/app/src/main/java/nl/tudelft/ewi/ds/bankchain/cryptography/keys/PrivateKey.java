884326d532a09b2349145d5ba81f0be475e88e45
Matthijs Kok <matthijs.kok@me.com>
2017-05-07 13:55:55 +0200

    Add signature verification to further mitigate attacks.

--- BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/keys/PrivateKey.java
+++ BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/keys/PrivateKey.java
@@ -8,5 +8,10 @@ public abstract class PrivateKey extends Key {
 
     public abstract String decodeMessage(String message);
 
+    /**
+     * Encrypts the SHA256 hash of the message with this private key.
+     * @param message the message to sign
+     * @return a String representing the signature.
+     */
     public abstract String signMessage(String message);
 }
