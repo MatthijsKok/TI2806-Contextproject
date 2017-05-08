063235852fc3d07b58aa0a70086066a3efb4934e
Matthijs Kok <matthijs.kok@me.com>
2017-05-08 10:45:45 +0200

    Document the crypto classes.

--- BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/Challenge.java
+++ BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/Challenge.java
@@ -10,8 +10,8 @@ import nl.tudelft.ewi.ds.bankchain.cryptography.keys.PublicKey;
 
 /**
  * Challenge class.
- * Will be able to encrypt a given random string with a public key.
- * Will also be able to check any Response on it's Challenge.
+ * Signs a given random string with a private key, and encrypts it with a responder's public key.
+ * Will also be able to check any Response on its Challenge.
  */
 public class Challenge {
 
@@ -22,6 +22,12 @@ public class Challenge {
     private String encryptedSecret;
     private PrivateKey challengerPrivateKey;
 
+    /**
+     * Create a Challenge.
+     * This will generate a secret, sign it and encrypt it with the responders public key.
+     * @param challengerPrivateKey
+     * @param responderPublicKey The responders public key to encrypt the secret with.
+     */
     public Challenge(PrivateKey challengerPrivateKey, PublicKey responderPublicKey) {
         this.challengerPrivateKey = challengerPrivateKey;
 
@@ -30,15 +36,26 @@ public class Challenge {
         encryptChallenge(responderPublicKey);
     }
 
+    /**
+     * Uses SecureRandom to generate a seed.
+     * This seed is encoded to the secret as a base64 String.
+     */
     private void generateChallenge() {
         this.seed = new SecureRandom().generateSeed(SEED_LENGTH);
         this.secret = Base64.encodeToString(seed, Base64.NO_WRAP);
     }
 
+    /**
+     * Create a signature of the secret using your private and store it with the object.
+     */
     private void signChallenge() {
         this.signature = challengerPrivateKey.signMessage(secret);
     }
 
+    /**
+     * Encrypt the secret using the challengers public key.
+     * @param responderPublicKey The challengers public key.
+     */
     private void encryptChallenge(PublicKey responderPublicKey) {
         this.encryptedSecret = responderPublicKey.encryptMessage(secret);
     }
@@ -55,6 +72,12 @@ public class Challenge {
         return this.challengerPrivateKey.getPublicKey();
     }
 
+    /**
+     * Verifies whether the signature of the Response is valid,
+     * and the decrypted secret equals our secret.
+     * @param response The Response object to verify.
+     * @return A boolean whether the response is valid.
+     */
     public boolean verifyResponse(Response response) {
         try {
             verifyResponseSignature(response);
@@ -64,6 +87,13 @@ public class Challenge {
         return secret.equals(challengerPrivateKey.decodeMessage(response.getEncryptedResponse()));
     }
 
+    /**
+     * Verifies the signature of the response.
+     * @param response The Response to verify the signature of
+     * @return Whether the signature is valid.
+     * @throws SignatureException If the signature is invalid.
+     * Invalid signatures are signs of malicious activity and therefore throw an Exception.
+     */
     private boolean verifyResponseSignature(Response response) throws SignatureException {
         if (!response.getPublicKey().verifySignedMessage(secret, response.getSignature())) {
             throw new SignatureException("The signature of the Response is invalid!");
--- BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/keys/PublicKey.java
+++ BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/keys/PublicKey.java
@@ -1,7 +1,18 @@
 package nl.tudelft.ewi.ds.bankchain.cryptography.keys;
 
+/**
+ * The PublicKey class.
+ * Public keys verify the signature of any message signed by its private key.
+ * Also can encrypt any message, so that only the private key can decrypt it.
+ */
 public abstract class PublicKey extends Key {
 
+    /**
+     * Encrypt a message with this public key.
+     * This encrypted message will be able to be decrypted by the corresponding private key.
+     * @param message the message String to encrypt.
+     * @return The encrypted message String.
+     */
     public abstract String encryptMessage(String message);
 
     /**
