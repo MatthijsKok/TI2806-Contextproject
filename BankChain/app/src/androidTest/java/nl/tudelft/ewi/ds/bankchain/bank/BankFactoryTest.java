2438b597a64e780a6c62dde84dbec665cce7333e
Jos Kuijpers <jkuijpers@jarvix.nl>
2017-05-19 12:04:02 +0200

    Fix typi

--- BankChain/app/src/androidTest/java/nl/tudelft/ewi/ds/bankchain/bank/BankFactoryTest.java
+++ BankChain/app/src/androidTest/java/nl/tudelft/ewi/ds/bankchain/bank/BankFactoryTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertTrue;
  */
 public class BankFactoryTest {
     @Test
-    public void testUnknoqnBankEnvironment() throws Exception {
+    public void testUnknownBankEnvironment() throws Exception {
         Bank bank;
         Environment env;
 
