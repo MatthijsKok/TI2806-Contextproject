07d20ec13df40de4c671d1b9a4fc17ad862fe393
Matthijs Kok <matthijs.kok@me.com>
2017-06-01 10:42:48 +0200

    remove kotlin

---
+++ BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/Verification.java
@@ -0,0 +1,43 @@
+package nl.tudelft.ewi.ds.bankchain.cryptography;
+
+
+import java.security.PublicKey;
+
+import nl.tudelft.ewi.ds.bankchain.bank.Account;
+
+public class Verification {
+
+    private Account account;
+    private PublicKey publicKey;
+
+
+    public Verification(Account account, PublicKey publicKey) {
+        this.account = account;
+        this.publicKey = publicKey;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(PublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Verification) {
+            Verification that = (Verification) obj;
+            return this.publicKey.equals(that.publicKey) && this.account.equals(that.account);
+        }
+        return false;
+    }
+}
--- BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/Verification.kt
+++
@@ -1,8 +0,0 @@
-package nl.tudelft.ewi.ds.bankchain.cryptography
-
-
-import java.security.PublicKey
-
-import nl.tudelft.ewi.ds.bankchain.bank.Account
-
-class Verification(var account: Account, var publicKey: PublicKey)
