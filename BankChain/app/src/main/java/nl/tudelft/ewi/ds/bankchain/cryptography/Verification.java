d800c5525b516f32bcffd992a620baec24c37474
Matthijs Kok <matthijs.kok@me.com>
2017-06-01 11:51:22 +0200

    simplify Verification

--- BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/Verification.java
+++ BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/cryptography/Verification.java
@@ -7,43 +7,11 @@ import nl.tudelft.ewi.ds.bankchain.bank.Account;
 
 public class Verification {
 
-    private Account account;
-    private PublicKey publicKey;
-
+    public Account account;
+    public PublicKey publicKey;
 
     public Verification(Account account, PublicKey publicKey) {
         this.account = account;
         this.publicKey = publicKey;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    public PublicKey getPublicKey() {
-        return publicKey;
-    }
-
-    public void setPublicKey(PublicKey publicKey) {
-        this.publicKey = publicKey;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Verification) {
-            Verification that = (Verification) obj;
-            return this.publicKey.equals(that.publicKey) && this.account.equals(that.account);
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        assert false : "hashCode not designed";
-        return 42; // any arbitrary constant will do
     }
 }
