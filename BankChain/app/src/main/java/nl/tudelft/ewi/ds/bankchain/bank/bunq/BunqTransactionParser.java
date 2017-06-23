a95add72b539967a7ab0a81548e4bf8ee3e2eb46
Matthijs Kok <matthijs.kok@me.com>
2017-06-23 15:38:31 +0200

    Rewrite TransactionParser

--- BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/bank/bunq/BunqTransactionParser.java
+++ BankChain/app/src/main/java/nl/tudelft/ewi/ds/bankchain/bank/bunq/BunqTransactionParser.java
@@ -6,13 +6,13 @@ import java.security.InvalidKeyException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SignatureException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Currency;
+import java.util.stream.Collectors;
 
-import nl.tudelft.ewi.ds.bankchain.bank.Bank;
+import nl.tudelft.ewi.ds.bankchain.BankTeller;
 import nl.tudelft.ewi.ds.bankchain.bank.Transaction;
 import nl.tudelft.ewi.ds.bankchain.bank.TransactionParser;
+import nl.tudelft.ewi.ds.bankchain.blockchain.Blockchain;
 import nl.tudelft.ewi.ds.bankchain.cryptography.Verification;
 
 import static nl.tudelft.ewi.ds.bankchain.cryptography.ChallengeResponse.createResponse;
@@ -23,49 +23,39 @@ import static nl.tudelft.ewi.ds.bankchain.cryptography.ChallengeResponse.isValid
 public class BunqTransactionParser implements TransactionParser {
 
     @Override
-    public void respondToPendingChallenges(Bank bank, PrivateKey privateKey, Collection<Transaction> transactionCollection) {
-        for (Transaction transaction: transactionCollection) {
-            String description = transaction.getDescription();
-            if (!isValidDescriptionFormat(description)) {
-                continue;
-            }
-            PublicKey publicKey = getPublicKeyForTransaction(transaction);
-            if (isValidChallenge(description, publicKey)) {
-                try {
-                    String response = createResponse(description, privateKey);
-                    sendTransaction(bank, transaction, response);
-                } catch (SignatureException | InvalidKeyException e) {
-                    Log.e("Transaction failed!", e.getLocalizedMessage());
-                }
-            }
-        }
-    }
+    public void respondToPendingChallenges(PrivateKey privateKey, Collection<Transaction> transactions, long cutoffTime) {
+        BankTeller teller = BankTeller.getBankTeller();
+        Blockchain chain = teller.getBlockchain();
 
-    private void sendTransaction(Bank bank, Transaction counterTransaction, String response) {
-        BunqTransaction transaction = new BunqTransaction(-0.01f, counterTransaction.getAccount(), counterTransaction.getCounterAccount(), Currency.getInstance("EUR"), response);
-        bank.sendTransaction(transaction);
+        transactions.stream()
+                .filter(t -> t.getValue() > 0)
+                .filter(t -> t.getDate().getTime() > cutoffTime)
+                .filter(t -> isValidDescriptionFormat(t.getDescription()))
+                .filter(t -> isValidChallenge(t.getDescription(), chain.getPublicKeyForIBAN(t.getCounterAccount().getIban())))
+                .forEach(t -> {
+                    try {
+                        teller.sendCent(t.getCounterAccount().getIban(),
+                                t.getCounterAccount().getParty().getName(),
+                                createResponse(t.getDescription(), privateKey));
+                    } catch (SignatureException | InvalidKeyException e) {
+                        Log.e("Transaction failed!", e.getLocalizedMessage());
+                    }
+                });
     }
 
     @Override
     public Collection<Verification> getVerifiedAccountsInTransactionList(Collection<Transaction> transactions) {
-        Collection<Verification> verifications = new ArrayList<>();
-        for (Transaction transaction: transactions) {
-            String description = transaction.getDescription();
-            if (!isValidDescriptionFormat(description)) {
-                continue;
-            }
-            PublicKey publicKey = getPublicKeyForTransaction(transaction);
-            if (isValidResponse(description, publicKey) || isValidChallenge(description, publicKey)) {
-                verifications.add(new Verification(
-                        transaction.getCounterAccount(),
-                        publicKey));
-            }
-        }
-        return verifications;
-    }
+        BankTeller bankTeller = BankTeller.getBankTeller();
+        Blockchain blockchain = bankTeller.getBlockchain();
 
-    private PublicKey getPublicKeyForTransaction(Transaction transaction) {
-        //TODO implement method
-        return null;
+        return transactions.stream()
+                .filter(t -> isValidDescriptionFormat(t.getDescription()))
+                .filter(t -> {
+                    PublicKey key = blockchain.getPublicKeyForIBAN(t.getCounterAccount().getIban());
+                    String description = t.getDescription();
+                    return isValidChallenge(description, key) || isValidResponse(description, key);
+                })
+                .map(t -> new Verification(t.getCounterAccount(), blockchain.getPublicKeyForIBAN(t.getCounterAccount().getIban())))
+                .collect(Collectors.toList());
     }
 }
