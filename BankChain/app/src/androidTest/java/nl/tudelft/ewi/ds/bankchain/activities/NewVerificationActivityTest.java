5d59c833d80e3061e34e5038521ac1d43c2433d4
IshaD <ishadijcks@gmail.com>
2017-06-23 13:09:44 +0200

    Add travis ignore.

--- BankChain/app/src/androidTest/java/nl/tudelft/ewi/ds/bankchain/activities/NewVerificationActivityTest.java
+++ BankChain/app/src/androidTest/java/nl/tudelft/ewi/ds/bankchain/activities/NewVerificationActivityTest.java
@@ -9,6 +9,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
+//TRAVIS import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
